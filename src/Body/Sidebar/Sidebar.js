3eceba20b463369b78040782232affe22b526f5a
Mert Önalan <mert.onalan1@gmail.com>
2022-07-24 23:57:55 +0300

    Sidebar design changes

--- src/Body/Sidebar/Sidebar.js
+++ src/Body/Sidebar/Sidebar.js
@@ -5,10 +5,10 @@ import {  useDispatch } from 'react-redux';
 import {activeUser} from '../../actions/actions'
 
 let userBots= [
-    {id: 1, name: 'Thomas Collin', photoUrl : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', photoAlt:'user1' },
-    {id: 2, name: 'Carly Gilfoy', photoUrl : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', photoAlt:'user1' },
-    {id: 3, name: 'Glenn Bremen', photoUrl : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', photoAlt:'user1' },
-    {id: 4, name: 'Su Yilmaz', photoUrl : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', photoAlt:'user1' },
+    {id: 1, name: 'Thomas Collin', photoUrl : 'https://images.pexels.com/photos/9607206/pexels-photo-9607206.jpeg?auto=compress&cs=tinysrgb&w=1600', photoAlt:'user1' },
+    {id: 2, name: 'Carly Gilfoy', photoUrl : 'https://images.pexels.com/photos/11900126/pexels-photo-11900126.jpeg?auto=compress&cs=tinysrgb&w=1600', photoAlt:'user2' },
+    {id: 3, name: 'Glenn Bremen', photoUrl : 'https://images.pexels.com/photos/5591671/pexels-photo-5591671.jpeg?auto=compress&cs=tinysrgb&w=1600', photoAlt:'user3' },
+    {id: 4, name: 'Enes Yilmaz', photoUrl : 'https://images.pexels.com/photos/7561911/pexels-photo-7561911.jpeg?auto=compress&cs=tinysrgb&w=1600', photoAlt:'user4' },
 ]
 
 const Sidebar = () => {
