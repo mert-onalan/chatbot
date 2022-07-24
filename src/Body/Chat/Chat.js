8e58e69b9e6be9e005889d50e6850c6dca47f645
Mert Önalan <mert.onalan1@gmail.com>
2022-07-25 00:01:55 +0300

    Chat auto scroll and design change

--- src/Body/Chat/Chat.js
+++ src/Body/Chat/Chat.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Inputt from './Input';
-import './Chat.scss'
 import { useSelector } from 'react-redux';
+import './Chat.scss'
 
 const Chat = () => {
-    const selectedUser = useSelector(state => state.activeUse);
 
+    const endOfMessages = useRef(null);
+    const selectedUser = useSelector(state => state.activeUse);
     const selector = useSelector(state => selectedUser === 1 ? state.firstUser : selectedUser === 2 ? state.secondUser : selectedUser === 3 ? state.thirdUser : selectedUser === 4 ? state.fourthUser : state.firstUser);
+
+    useEffect(()=>{
+        endOfMessages.current?.scrollIntoView({behavior: "smooth"});
+    },[selector])
 
     return (
         <div className='chat-area'>
             {
                 selector && selector.length > 0 && selector.map((message,index) =>  {
                     return(
-                        <div id={message.identifier === 0 ? 'me' : 'bot'} className='message'>
-                            <div className='message-content' key={index}>{message.texts}</div>
+                        <div key={index} id={message.identifier === 0 ? 'me' : 'bot'} className='message'>
+                            <div className='message-content'>{message.texts}</div>
                         </div>
                     )})
             }
+            <div ref={endOfMessages} />
+            
             <Inputt />
         </div>
     );
