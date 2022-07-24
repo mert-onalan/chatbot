06c988fe125acf2c2f374cfc1076496e5bad07ed
Mert Önalan <mert.onalan1@gmail.com>
2022-07-24 23:57:22 +0300

    Header design changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,10 +4,16 @@ const Header = () => {
     return (
         <div className="header">
             <div className='header-left'>
-                <div className='avatar' />
+                <div className='avatar'> 
+                <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" alt="user0" />
+                </div>
+                <div className='user-info' >
+                    <div> Jeff Müller </div>
+                    <div className='status' />
+                </div>
             </div>
             <div className='header-middle'>
-                <input placeholder="Search in messanger"   />
+                ChatBot
             </div>
             <div className='header-right'>
             </div>
